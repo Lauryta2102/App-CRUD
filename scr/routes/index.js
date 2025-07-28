71cd582a375666c90ebbf42d031a907a90b01c47
laura ibaceta <lauraiaceta@gmail.com>
2025-07-27 21:57:34 -0300

    agrego controladores y vistas

--- scr/routes/index.js
+++ scr/routes/index.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const controllers = require('../controllers/controlers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/', function(req, res, next) {
-  const db = req.app.get("db");
+router.get('/personas', controllers.listaPersonas);
 
-  db.query('SELECT * FROM usuarios', function(err, results) {
-    if (err) {
-      return next(err);
-    }
-    res.render('index', { usuarios: results });
-  });
-});
+router.get('/agregarPersona', controllers.agregarPersona)
+router.post('/agregarPersona',controllers.postAgregarPersona);
+
+router.get('/edit/:id', controllers.getEditarPersona);
+router.post('/update/:id', controllers.postUpdatePersona);
+
+router.get('/delete/:id', controllers.getDeletePersona);
+router.post('/delete/:id', controllers.postDeletePersona);
+
+router.get('/buscar', controllers.buscarPersona);
+router.post('/resultados', controllers.buscarPersonaResultados);
 
 module.exports = router;
